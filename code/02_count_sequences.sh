712bf4452320cce0d0a849af3241c0e94e9254e7
Leila <lesmaeili@usfca.edu>
2018-09-26 19:48:42 -0700

    second script

---
+++ code/02_count_sequences.sh
@@ -0,0 +1,15 @@
+#!/bin/#!/usr/bin/env bash
+#making for 04_HW_for_loops
+echo
+echo "Looping through files"
+echo
+
+for file; do
+  Name1="File Name "
+  Name2=$file
+  LineCount=$(grep -c "^>" $file)
+  Name3="Has "
+  Name4="Lines"
+  echo $Name1 $Name2 $Name3 $LineCount $Name4
+  echo
+done
