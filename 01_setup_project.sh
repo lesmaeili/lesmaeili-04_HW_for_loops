afe3970dcb0c6b233488ad8e0f820cfb91aa3f24
Leila <lesmaeili@usfca.edu>
2018-09-26 19:08:01 -0700

    finished first script

---
+++ 01_setup_project.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Leila lesmaeili
+# Making a directory contaiting
+mkdir code data data/raw_data output output/tables output/figures
+echo "Making directories"
+# Download the fasta files into raw_daa subdirectory
+echo " Downloading the fasta files into raw_data subdirectory"
+cd data
+cd raw_data
+curl -L http://npk.io/PUlBaY+ -o fasta_archive.zip
+echo " Unzip fasta files "
+unzip fasta_archive.zip
+echo "Done Downloading fasta files"
+echo " Remove fasta file archive folder"
+rm fasta_archive.zip
