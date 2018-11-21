a790512f2c66459eae01d29ce1137fed7978eddc
Leila Esmaeili <lesmaeili@dons.usfca.edu>
2018-11-21 12:36:01 -0800

    correcting Cd erros

--- HW4_Bash_Setup.sh
+++ HW4_Bash_Setup.sh
@@ -16,15 +16,16 @@ mkdir code data data/raw_data output output/tables output/figures
 #echo "Adding a blank README.md--Please fill me later!"
 # Download the fasta files into raw_daa subdirectory
 echo " Downloading the fasta files into raw_data subdirectory"
-cd data
-cd raw_data
-curl -L http://npk.io/PUlBaY+ -o fasta_archive.zip
-#cd target/path && { curl -O http://npk.io/PUlBaY+ -o fasta_archive.zip ; cd -; }
+#cd data
+#cd raw_data
+#curl -L http://npk.io/PUlBaY+ -o fasta_archive.zip
+cd data/raw_data && { curl -O http://npk.io/PUlBaY+ -o fasta_archive.zip ; cd -; }
 echo " Unzip fasta files "
-unzip fasta_archive.zip
+#unzip fasta_archive.zip
+unzip data/raw_data/fasta_archive.zip
 echo "Done Downloading fasta files"
 echo " Remove fasta file archive folder"
-rm fasta_archive.zip
+rm data/raw_data/fasta_archive.zip
 echo " go into empty files and create git keep files"
 touch .gitkeep
 echo " Done creating. gitkeep files"
