ac8ae255e5c406fb79261d2ded6f15fb250c124c
Leila Esmaeili <lesmaeili@dons.usfca.edu>
2018-11-23 09:09:29 -0800

    fixedset_up script and passed shellcheck

--- 01_setup_project.sh
+++ 01_setup_project.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 # Leila lesmaeili
+# November 22, 2018
+# Bioinformatics HW4 Re-submission
+# lesmaeili@dons.usfca.edu
+
 # Making a directory
 mkdir code data data/raw_data output output/tables output/figures
 echo "Making directories"
 # Download the fasta files into raw_data subdirectory
-#echo " Downloading the fasta files into raw_data subdirectory"
 curl -L http://npk.io/PUlBaY+ -o data/raw_data/fasta_archive.zip
-echo " Unzip fasta files "
+
+#Unzip fasta files
 unzip data/raw_data/fasta_archive.zip
-echo "Done Downloading fasta files"
-echo " Remove fasta file archive folder"
+echo "Unzip fasta files"
+
+# Remove fasta files archive folder
 rm data/raw_data/fasta_archive.zip
+echo " Remove fasta file archive folder"
+
+# Creating .gitkeep
 touch output/figures/.gitkeep output/tables/.gitkeep
+echo "Creating .gitkeep"
--- code/02_count_sequences.sh
+++ code/02_count_sequences.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-#making for 04_HW_for_loops
+#m 04_HW_for_loops
+# Leila lesmaeili
+# lesmaeili@dons.usfca.edu
+# November 22, 2018 Resubmission
+#The goal of this bash  script is to count the number of sequences in
+#an arbitrary number of fasta files and report that
+# information to the user.
 echo
 echo "Looping through files"
 echo
